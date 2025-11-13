83e73a27511cd43bda4c0e0ed232a1ebb6abc185
error404ttk <oanyc.errorrectifier404@gmail.com>
2025-11-13 13:37:33 +0700

    Move sensitive data to .env file and update configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,8 +11,13 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 3001,
-        // Add any other environment variables your backend needs here
+        DB_HOST: process.env.DB_HOST,
+        DB_PORT: process.env.DB_PORT,
+        DB_USER: process.env.DB_USER,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_DATABASE: process.env.DB_DATABASE,
       },
+      env_file: "./server/.env",
     },
     {
       name: "moph-portal-frontend",
